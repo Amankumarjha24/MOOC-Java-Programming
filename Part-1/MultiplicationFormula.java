aebff847b4194bbd2f9c2f5d6c0915e84fbc64b1
Aman kumar <ajha00821@gmail.com>
2026-01-28 16:47:47 +0530

    Add files via upload

---
+++ Part-1/MultiplicationFormula.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class MultiplicationFormula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+//        System.out.println("Give the first number:");
+//        int number1 = Integer.valueOf(scanner.nextLine());
+//        System.out.println("Give the second number:");
+//        int number2 = Integer.valueOf(scanner.nextLine());
+//        System.out.println(number1+" * "+ number2 + " = "+ (number1*number2));
+
+
+//        Try
+        System.out.println("Enter one number please");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter second number please");
+        int num2 = Integer.valueOf(scanner.nextLine());
+        System.out.println(num1+"*"+num2+" = "+ (num1*num2));
+        
+        
+
+    }
+}
